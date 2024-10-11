29ce4e7b49cc5c56f77f9958347c17169989a2c9
mark <piringerm@kkszki.hu>
2024-10-11 12:58:54 +0200

    első kettő kész

--- Program.cs
+++ Program.cs
@@ -8,9 +8,28 @@ namespace napszakok
 {
     internal class Program
     {
-        public static void udvozol() 
-        { 
-        
+        public static void udvozol()
+        {
+            DateTime ido = DateTime.Now;
+            Console.WriteLine(ido.ToString());
+            if (0 <= ido.Hour && ido.Hour >= 9) {
+                Console.WriteLine("jó reggelt");
+            }
+            else if (10 <= ido.Hour && ido.Hour >= 12) {
+                Console.WriteLine("szép napot");
+            }
+            else if (12 <= ido.Hour && ido.Hour >= 16) {
+                Console.WriteLine("jó déutánt");
+            }
+            else if (17 <= ido.Hour && ido.Hour >= 19)
+            {
+                Console.WriteLine("szép estét");
+            }
+            else if (20 <= ido.Hour && ido.Hour >= 24)
+            {
+                Console.WriteLine("jó éjszakát");
+            }
+            Console.ReadKey();
         }
         public static void kamat()
         {
@@ -22,8 +41,9 @@ namespace napszakok
         }
         static void Main(string[] args)
         {
-
-
+            udvozol();
+            kamat(); 
+            futas();
         }
     }
 }

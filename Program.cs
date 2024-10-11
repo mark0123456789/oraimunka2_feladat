a9edb17d4d059728925990f17b85aabf657fe8d1
mark <piringerm@kkszki.hu>
2024-10-11 13:27:50 +0200

    első kettő kész

--- Program.cs
+++ Program.cs
@@ -31,8 +31,24 @@ namespace napszakok
             }
             Console.ReadKey();
         }
-        public static void kamat()
+        public static Double kamat()
         {
+            double kamatozas;
+            Console.Write("mennyi időre szeretnéd ezt a pénzt be tenni(add meg években): ");
+            int kamatido =Convert.ToInt32(Console.ReadLine());
+            Console.Write("mekkora összeget szeretnél be venni: ");
+            int kamatosszeg =Convert.ToInt32(Console.ReadLine());
+            Console.Write("mekkora a kamat százaléka: ");
+            int kamat =Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < kamatido;i++) 
+            {
+               kamatozas = (kamatosszeg / kamat)*100 + kamatosszeg;
+            }
+            Console.WriteLine(" {0} FT-t van a számládon {1} évnyi kamatozás után",kamatozas,kamatido);
+          return kamatozas;
+
+
+
 
         }
         public static void futas()
@@ -41,7 +57,7 @@ namespace napszakok
         }
         static void Main(string[] args)
         {
-            udvozol();
+           // udvozol();
             kamat(); 
             futas();
         }

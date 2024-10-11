78853270f2d43253388bb3baa1afa9ca12e9371d
mark <piringerm@kkszki.hu>
2024-10-11 11:53:47 +0200

    kezdetek

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace napszakok
+{
+    internal class Program
+    {
+        public static void udvozol() 
+        { 
+        
+        }
+        public static void kamat()
+        {
+
+        }
+        public static void futas()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
